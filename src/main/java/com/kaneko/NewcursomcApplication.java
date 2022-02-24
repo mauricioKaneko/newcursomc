076226d525812c77e2b27ad0f23b27875e913589
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-02-24 09:55:34 -0300

    Criando Projeto

---
+++ src/main/java/com/kaneko/NewcursomcApplication.java
@@ -0,0 +1,13 @@
+package com.kaneko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcursomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaneko/NewcursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaneko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
